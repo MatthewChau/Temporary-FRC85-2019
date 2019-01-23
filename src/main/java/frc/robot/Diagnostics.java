f4965f7b485e8dbdfb596f96f0cbb60b7972d0b0
Jasmine <mariejaz2003@gmail.com>
2019-01-22 19:48:51 -0500

    Simple diagnostics
    basically the same as last years w/o a buch of values, so we'll add those later

--- src/main/java/frc/robot/Diagnostics.java
+++ src/main/java/frc/robot/Diagnostics.java
@@ -10,11 +10,66 @@ package frc.robot;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.text.SimpleDateFormat;
 
-/**
- * Add your docs here.
- */
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Diagnostics {
 
-    
+    File log;
+    BufferedWriter out = null;
+
+    private int _placeHolder = 0;
+
+    public void init() {
+        try {
+            close();
+
+            _placeHolder = 0;
+
+            String date = new SimpleDateFormat("yyyy-MM-ddy HHmmss")
+                    .format(new java.util.Date(System.currentTimeMillis()));
+            log = new File("home/lvuser/log " + date + ".csv");
+            log.createNewFile();
+            out = new BufferedWriter(new FileWriter(log, true));
+            out.append("Time,Match Time");
+            out.newLine();
+        } catch (Exception ex) {
+            System.out.println("Error creating log file: " + ex.toString());
+        }
+    }
+
+    /**
+     * runs repeatedly to get values and such
+     */
+    public void log() {
+        try{
+            if (out == null) {
+                init();
+            }
+            _placeHolder++;
+            String time = Integer.toString(_placeHolder);
+            String matchTime = Double.toString(DriverStation.getInstance().getMatchTime());
+
+            //plus otros gets
+
+            out.append(time + "," + matchTime);
+
+            out.newLine();
+        } catch (Exception ex) {
+            System.out.println("Error writing diagnostic log: " + ex.toString());
+        }
+
+    }
+
+    public void close() {
+        if (out != null) {
+            try {
+                out.close();
+                out = null;
+            } catch (Exception ex) {
+                System.out.println("Error closing file: " + ex.toString());
+            }
+        }
+    }
 }
