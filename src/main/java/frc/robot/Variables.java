54268ad722cab0caadd5622a293f7ae186b5a0fe
kyleh8892 <46581117+kyleh8892@users.noreply.github.com>
2019-01-24 17:19:51 -0500

    Update Variables.java

--- src/main/java/frc/robot/Variables.java
+++ src/main/java/frc/robot/Variables.java
@@ -33,6 +33,9 @@ public class Variables {
         SmartDashboard.putNumber("Right Front Percent", DriveTrain.getInstance().getRightFrontPercent());
         SmartDashboard.putNumber("Right Back Percent", DriveTrain.getInstance().getRightBackPercent());
         SmartDashboard.putNumber("Width", Vision.distance());
+        //SmartDashboard.putNumber("Width2", Vision.distance()[1]);
+        //SmartDashboard.putNumber("Width3", Vision.distance()[2]);
+        //SmartDashboard.putNumber("Width4", Vision.distance()[3]);
 
     }
 
