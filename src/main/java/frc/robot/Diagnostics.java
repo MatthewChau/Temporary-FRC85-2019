486bb12ad1008ab28c613c4acbae67776ceda4b3
Jasmine <mariejaz2003@gmail.com>
2019-02-04 16:46:56 -0500

    Update Diagnostics.java
    added drive train motor %s, prox sensors and ypr

--- src/main/java/frc/robot/Diagnostics.java
+++ src/main/java/frc/robot/Diagnostics.java
@@ -13,6 +13,9 @@ import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.sensors.IMU;
+import frc.robot.sensors.ProxSensors;
+import frc.robot.subsystems.DriveTrain;
 
 public class Diagnostics {
 
@@ -32,7 +35,12 @@ public class Diagnostics {
             log = new File("home/lvuser/log " + date + ".csv");
             log.createNewFile();
             out = new BufferedWriter(new FileWriter(log, true));
-            out.append("Time,Match Time");
+            out.append("Time,Match Time,"
+                  + "Left Front Motor,Left Back Motor,Right Front Motor,Right Back Motor,"
+                  + "Lift Top Limit,Lift Bottom Limit,Lift Front Limit,Lift Rear Limit,"
+                  + "Intake Top Limit,Intake Bottom Limit,"
+                  + "Yaw,Pitch,Roll,"
+                  + ",");
             out.newLine();
         } catch (Exception ex) {
             System.out.println("Error creating log file: " + ex.toString());
@@ -50,10 +58,31 @@ public class Diagnostics {
             _placeHolder++;
             String time = Integer.toString(_placeHolder);
             String matchTime = Double.toString(DriverStation.getInstance().getMatchTime());
+            
+            String leftFrontMotor = Double.toString(DriveTrain.getInstance().getLeftFrontPercent());
+            String leftBackMotor = Double.toString(DriveTrain.getInstance().getLeftBackPercent());
+            String rightFrontMotor = Double.toString(DriveTrain.getInstance().getRightFrontPercent());
+            String rightBackMotor = Double.toString(DriveTrain.getInstance().getRightBackPercent());
 
-            //plus oth get values
+            String liftTopLimit = Boolean.toString(ProxSensors.getInstance().getLiftTopLimit());
+            String liftBottomLimit = Boolean.toString(ProxSensors.getInstance().getLiftBottomLimit());
+            String liftFrontLimit = Boolean.toString(ProxSensors.getInstance().getLiftFrontLimit());
+            String liftRearLimit = Boolean.toString(ProxSensors.getInstance().getLiftRearLimit());
 
-            out.append(time + "," + matchTime);
+            String intakeTopLimit = Boolean.toString(ProxSensors.getInstance().getIntakeTopLimit());
+            String intakeBottomLimit = Boolean.toString(ProxSensors.getInstance().getIntakeBottomLimit());
+
+            String yaw = Double.toString(IMU.getInstance().getYaw());
+            String pitch = Double.toString(IMU.getInstance().getPitch());
+            String roll = Double.toString(IMU.getInstance().getRoll());
+
+
+            out.append(time + "," + matchTime + "," 
+                    + leftFrontMotor + "," + leftBackMotor + "," + rightFrontMotor + "," + rightBackMotor + ","
+                    + liftTopLimit + "," + liftBottomLimit + "," + liftFrontLimit + "," + liftRearLimit + ","
+                    + intakeTopLimit + "," + intakeBottomLimit + ","
+                    + yaw + "," + pitch + "," + roll + ","
+                    + ",");
 
             out.newLine();
         } catch (Exception ex) {
