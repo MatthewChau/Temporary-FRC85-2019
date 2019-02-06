ba186244a20a6d31fa0a19c5f66357345cf1227b
Jasmine <mariejaz2003@gmail.com>
2019-02-05 19:34:58 -0500

    naming stuff & oth stuff
    fixed some capitalization, and percent output and voltage for the motors might get used in diagnostics...

--- src/main/java/frc/robot/subsystems/BeltTrain.java
+++ src/main/java/frc/robot/subsystems/BeltTrain.java
@@ -16,12 +16,12 @@ public class BeltTrain extends Subsystem {
     
     public static BeltTrain _instance = null;
 
-    private TalonSRX _belttrainfrontmotor, _belttrainbackmotor;
+    private TalonSRX _beltTrainFrontMotor, _beltTrainBackMotor;
     private Solenoid _beltSolenoid;
 
     public BeltTrain() {
-        _belttrainfrontmotor = new TalonSRX(Addresses.BELTTRAIN_FRONT_MOTOR);
-        _belttrainbackmotor = new TalonSRX(Addresses.BELTTRAIN_BACK_MOTOR);
+        _beltTrainFrontMotor = new TalonSRX(Addresses.BELTTRAIN_FRONT_MOTOR);
+        _beltTrainBackMotor = new TalonSRX(Addresses.BELTTRAIN_BACK_MOTOR);
         
         _beltSolenoid = new Solenoid(Addresses.BELT_SOLENOID);
     }
@@ -47,9 +47,25 @@ public class BeltTrain extends Subsystem {
     }
 
     public void DriveBelt(double speed) {
-     _belttrainbackmotor.set(ControlMode.PercentOutput, speed);
-     _belttrainfrontmotor.set(ControlMode.PercentOutput, speed);
+        _beltTrainFrontMotor.set(ControlMode.PercentOutput, speed);
+        _beltTrainBackMotor.set(ControlMode.PercentOutput, speed);
+        
+    }
 
+    public double getFrontMotorPercent() {
+        return _beltTrainFrontMotor.getMotorOutputPercent();
+    }
+
+    public double getBackMotorPercent() {
+        return _beltTrainBackMotor.getMotorOutputPercent();
+    }
+
+    public double getFrontMotorVoltage() {
+        return _beltTrainFrontMotor.getMotorOutputVoltage();
+    }
+
+    public double getBackMotorVoltage() {
+        return _beltTrainBackMotor.getMotorOutputVoltage();
     }
      
 }
