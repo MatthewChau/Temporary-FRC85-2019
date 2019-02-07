a16cff8cc665c9f897f9f83b3ed81a63930c24b8
Matthew <MatthewChauA@gmail.com>
2019-02-07 16:54:46 -0500

    Revert "Fixed robot should run, but yours didn't error"
    This reverts commit b66c9618d03444c9119fb3cca433f242bddd1734.

--- src/main/java/frc/robot/commands/lift/LiftHorizontalWithJoystick.java
+++ src/main/java/frc/robot/commands/lift/LiftHorizontalWithJoystick.java
@@ -26,7 +26,7 @@ public class LiftHorizontalWithJoystick extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        //LiftHorizontal.getInstance().horizontalShift(OI.getInstance().getLiftHorizontal());
+        LiftHorizontal.getInstance().horizontalShift(OI.getInstance().getLiftHorizontal());
     }
 
     // Make this return true when this Command no longer needs to run execute()
