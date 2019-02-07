bef6b3d81f5a5e844fc29d88ca85965017f40c0f
Matthew <MatthewChauA@gmail.com>
2019-02-07 16:53:24 -0500

    Delete

--- src/main/java/frc/robot/commands/lift/HorizontalShift.java
+++ src/main/java/frc/robot/commands/lift/HorizontalShift.java
@@ -26,7 +26,6 @@ public class HorizontalShift extends Command {
     protected void initialize() {
         super.initialize();
         //Lift.getInstance().horizontalShift(_direction);
-        //Change to "Lift.getInstance().horizontalShift(_direction * OI.getInstance().getRawAxis)" or something like that if you wish to manipulate based on joystick axis.
     }
 
     @Override
