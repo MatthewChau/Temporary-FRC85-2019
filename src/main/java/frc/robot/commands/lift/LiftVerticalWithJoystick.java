b66c9618d03444c9119fb3cca433f242bddd1734
Matthew <MatthewChauA@gmail.com>
2019-02-07 16:54:22 -0500

    Fixed robot should run, but yours didn't error

--- src/main/java/frc/robot/commands/lift/LiftVerticalWithJoystick.java
+++ src/main/java/frc/robot/commands/lift/LiftVerticalWithJoystick.java
@@ -26,7 +26,7 @@ public class LiftVerticalWithJoystick extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-//        LiftVertical.getInstance().verticalShift(OI.getInstance().getLiftVertical());
+        //LiftVertical.getInstance().verticalShift(OI.getInstance().getLiftVertical());
 
     }
 
