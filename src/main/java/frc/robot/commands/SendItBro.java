c26967a850df966f89349aeb08ce2b3e75be85ce
kyleh8892 <46581117+kyleh8892@users.noreply.github.com>
2019-02-19 18:54:39 -0500

    actually go forward

--- src/main/java/frc/robot/commands/SendItBro.java
+++ src/main/java/frc/robot/commands/SendItBro.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class SendItBro extends CommandGroup {
 
-    double _speed[] = {0.5, 0, 0, 0};
+    double _speed[] = {0.0, 0.5, 0.0, 0.0};
 
     // i guess this is what we are doing for future reference
 
