f8e4b52fda05e4358477c52c82ba2c7e3afe25b3
kyleh8892 <46581117+kyleh8892@users.noreply.github.com>
2019-02-22 08:31:40 -0500

    General updates (Read Description)
    Increased Divetrain speed, decreased time, added reverse to square with hab, added comments.

--- src/main/java/frc/robot/commands/SendItBro.java
+++ src/main/java/frc/robot/commands/SendItBro.java
@@ -8,15 +8,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class SendItBro extends CommandGroup {
 
-    double _speed[] = {0.0, -0.5, 0.0, 0.0};
+    double _speed[] = {0.0, -0.7, 0.0, 0.0};
+    double _backspeed[] = {0.0, 0.7, 0.0, 0.0};
 
     // i guess this is what we are doing for future reference
 
     public SendItBro() {
         addSequential(new Wait(0.3));
         //addSequential(new HorizontalShift(0, 0.2)); // values subject to change
-        addParallel(new DriveSeconds(_speed, 5.0));
-        addSequential(new BeltTrainDrive(.5, 5.0));
+        addParallel(new DriveSeconds(_speed, 2.0)); //Drive belt and drivetrain forward off hab 
+        addSequential(new BeltTrainDrive(.5, 2.0));
+        addSequential(new DriveSeconds(_backspeed, 2.0)); //Reverse to square up with the hab 
     }
     
 }
