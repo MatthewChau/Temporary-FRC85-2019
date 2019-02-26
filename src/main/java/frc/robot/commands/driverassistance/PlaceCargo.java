f3a38a94134ee876392a53b014784c41ab582f8f
Nathan Wichers <bluroseai@hotmail.com>
2019-02-26 17:58:21 -0500

    fix some commands

--- src/main/java/frc/robot/commands/driverassistance/PlaceCargo.java
+++ src/main/java/frc/robot/commands/driverassistance/PlaceCargo.java
@@ -3,8 +3,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Variables;
 import frc.robot.commands.drivetrain.FollowTwoTarget;
 import frc.robot.commands.intake.IntakePosition;
-import frc.robot.commands.lift.HorizontalShift;
-import frc.robot.commands.lift.VerticalShift;
+import frc.robot.commands.lift.LiftVerticalPosition;
 import frc.robot.commands.drivetrain.DriveSeconds;
 import frc.robot.commands.drivetrain.FollowOneTarget;
 import frc.robot.commands.intake.ActivateIntake;
@@ -14,12 +13,12 @@ public class PlaceCargo extends CommandGroup {
     public PlaceCargo(int liftPosition, int rotateIntakeToThis) {
         addSequential(new FollowOneTarget(), 3);
         addSequential(new FollowTwoTarget(), 3);
-        addSequential(new VerticalShift(liftPosition, 0.3));
+        addSequential(new LiftVerticalPosition(liftPosition));
         addSequential(new IntakePosition(rotateIntakeToThis));
         addSequential(new ActivateIntake(-.5));
         addSequential(new Wait(3));
-        addSequential(new ActivateIntake(0));
+        addSequential(new ActivateIntake(Variables.getInstance().INTAKE_DEGREE_ONE));
         addParallel(new IntakePosition(0)); //Need to move lift horizontally backwards while at the same time set the flipper position back to zero
-        addSequential(new VerticalShift(0, 0.3));
+        addSequential(new LiftVerticalPosition(Variables.getInstance().CARGO_FLOOR));
     }
 }
--- src/main/java/frc/robot/commands/driverassistance/PlaceHatch.java
+++ src/main/java/frc/robot/commands/driverassistance/PlaceHatch.java
@@ -3,8 +3,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Variables;
 import frc.robot.commands.drivetrain.FollowTwoTarget;
 import frc.robot.commands.intake.IntakePosition;
-import frc.robot.commands.lift.HorizontalShift;
-import frc.robot.commands.lift.VerticalShift;
+import frc.robot.commands.lift.LiftHorizontalPosition;
+import frc.robot.commands.lift.LiftVerticalPosition;
 import frc.robot.commands.drivetrain.DriveSeconds;
 import frc.robot.commands.drivetrain.FollowOneTarget;;
 
@@ -12,11 +12,11 @@ public class PlaceHatch extends CommandGroup {
     public PlaceHatch(int liftPosition, int rotateIntakeToThis) {
         addSequential(new FollowOneTarget(), 3);
         addSequential(new FollowTwoTarget(), 3);
-        addSequential(new VerticalShift(liftPosition, 0.3));
+        addSequential(new LiftVerticalPosition(liftPosition));
         addSequential(new IntakePosition(rotateIntakeToThis));
-        addSequential(new HorizontalShift(9000, 0.25)); //9000 is just a random number for now
+        addSequential(new LiftHorizontalPosition(9000)); //9000 is just a random number for now
         addParallel(new IntakePosition(0)); //Need to move lift horizontally backwards while at the same time set the flipper position back to zero
-        addSequential(new HorizontalShift(0, 0.25));
-        addSequential(new VerticalShift(0, 0.3));
+        addSequential(new LiftHorizontalPosition(0));
+        addSequential(new LiftVerticalPosition(0));
     }
 }
