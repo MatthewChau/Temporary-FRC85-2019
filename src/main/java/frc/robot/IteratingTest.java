478fd370a549355ceb064e8b98139b6d84c8df2e
Nathan Wichers <bluroseai@hotmail.com>
2019-03-16 16:06:11 -0400

    Delete IteratingTest.java
    have no idea why this existed, maybe a debugging thing once upon a time?

--- src/main/java/frc/robot/IteratingTest.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-public class IteratingTest {
-    public static void main(String[] args) {
-        while (true) {
-            System.out.println(Vision.getInstance().twoTargetDistance());
-        }
-    }
-}
